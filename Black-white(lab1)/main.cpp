d07b9b6f045f1ae658a97e021574b94c4da9dc7b
Artem <klufone@mail.ru>
2017-02-18 09:40:54 +0300

    Initial Commit

---
+++ Black-white(lab1)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Black-white(lab1)
+//
+//  Created by артем on 18.02.17.
+//  Copyright © 2017 артем. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
