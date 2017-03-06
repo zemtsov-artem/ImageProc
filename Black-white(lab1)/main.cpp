1614dc2730a61ebf3baacfca08233ffd40d37a8e
Artem <klufone@mail.ru>
2017-03-06 06:55:42 +0300

    rework code, and use better practices

--- Black-white(lab1)/main.cpp
+++ Black-white(lab1)/main.cpp
@@ -1,3 +1,13 @@
+//
+//  rgbtogray.cpp
+//  Black-white(lab1)
+//
+//  Created by артем on 03.03.17.
+//  Copyright © 2017 артем. All rights reserved.
+//
+//Some describe
+//GI - gray image
+//Arr - array
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
@@ -17,8 +27,6 @@ int main( )
         return -1;
     }
     
-    cout<<img.cols<<" simpleImg cols"<<endl;
-    cout<<img.rows<<" simpleImg rows"<<endl;
     
     // rgb to gray scale
     Mat grayImg = intensity(img);
@@ -27,103 +35,102 @@ int main( )
     // declarate and init ptr and temrary min
     cv::Mat_<uchar> ptr = hist;
     cv::Mat_<uchar> GrayPtr = grayImg;
-
     // declarate arr
     uint *histArr = new uint[255];
-    // init arr
-    for (int i =0; i<256; i++) {
-        *(histArr+i) = 0;
-    }
-
-    // compare our values and tempMin
-    for (int i =0; i<grayImg.rows; i++) {
-        for (int j = 0; j<grayImg.cols; j++) {
-            *(histArr+GrayPtr(i,j))+=1;
-        }
-    }
-//    //Show array
-//    for (int i =0; i<256; i++) {
-//        cout<<*(histArr+i)<<" - "<<i<<endl;
-//    }
     
-    int tempMin = grayImg.rows*grayImg.cols;
-    int tempMax = 0;
-    int leftFlag = 256, rightFlag = 0;
+    // init arr and filling from gray image
+    initHistArr(histArr);
+    fillArrayWithValuesFromGI(histArr, grayImg);
     
-    // find min
-    for (int i =0; i<256; i++) {
-        if ( (*(histArr+i) < tempMin) ) {
-            tempMin = *(histArr+i);
-            rightFlag = i;
-        }
-    }
+    // the declaration of the flags of the describing min an max
+    uint leftFlag = 256, rightFlag = 0;
+    findMaxValueInHistArrayAndFixIndex(histArr, &leftFlag);
+    findMinValueInHistArrayAndFixIndex(histArr, &rightFlag);
     
-    // find max
-    for (int i =0; i<256; i++) {
-        if ( (*(histArr+i) > tempMax)) {
-            tempMax = *(histArr+i);
-            leftFlag = i;
-        }
-    }
+    // declarate
+    ushort barrierObtainedByTheMethodOfTheTriangle = -1;
+    ushort SimpleBarrier = (rightFlag+leftFlag)/2;
     
-    cout<< tempMin<<" - min and his num = "<<rightFlag<<endl;
-    cout<< tempMax<<" - max and his num = "<<leftFlag<<endl;
-    
-    //уравнение прямой (x-x1)/(x2-x1) = (y-y1)/(y2-y1)
-    
-    int maxDistance = 0;
-    int unknownIntesity = -1;
-    
-    for (int i = leftFlag; i<(rightFlag-leftFlag); i++) {
-        double tempValue =
-            abs(
-                (leftFlag+i)/(rightFlag-leftFlag)+
-                *(histArr+leftFlag+i)/(*(histArr+rightFlag) - *(histArr+leftFlag) ) -
-                leftFlag/(rightFlag-leftFlag) +
-                *(histArr+leftFlag)/(*(histArr+rightFlag) - *(histArr + leftFlag) )
-                )  /
-            sqrt(
-                 (1 / pow(rightFlag-leftFlag, 2) ) +
-                 (1 / pow((*(histArr+rightFlag) - *(histArr+leftFlag)),2) )
-                 );
-        if ( ((int)tempValue)>maxDistance ){
-            maxDistance = (int)tempValue;
-            unknownIntesity = leftFlag + i ;
-        }
-        
-    }
+    barrierObtainedByTheMethodOfTheTriangle = findMaxDistanceFromLineToSomePoint(
+                                                                                 leftFlag,
+                                                                                 rightFlag,
+                                                                                 histArr);
     
     
-    cout<<maxDistance<<" - maxDistance"<<endl;
-    if ((unknownIntesity >=0 ) && (unknownIntesity<=255)) {
-        cout<<unknownIntesity<<" -unknownIntensity"<<endl;
+    if ( checkCorrect(barrierObtainedByTheMethodOfTheTriangle,0,255)) {
+        cout<<barrierObtainedByTheMethodOfTheTriangle<<" - unknownBarrier"<<endl;
     }
     else {
-        cout<<"Wrong instensity"<<endl;
+        cout<<"Wrong barrier"<<endl;
     }
+    // declarate new mat to save new binary images
+    Mat CoolBlackAndWhiteImage,SimpleBlackAndWhiteImage;
     
-    Mat blackAndWhiteImage;
-
-    grayImg.copyTo(blackAndWhiteImage);
-    Mat_<uchar> blackAndWhiteImagePtr = blackAndWhiteImage;
+    grayImg.copyTo(CoolBlackAndWhiteImage);
+    grayImg.copyTo(SimpleBlackAndWhiteImage);
+    Mat_<uchar> blackAndWhiteImagePtr = CoolBlackAndWhiteImage;
+    Mat_<uchar> SimpleBlackAndWhiteImagePtr = SimpleBlackAndWhiteImage;
     
-    
-    cout<<img.cols<<" - cols"<<endl;
-    cout<<img.rows<<" - rows"<<endl;
-    
+  
+    // our method to convert image to black and white
     for (int i  = 0 ; i < blackAndWhiteImagePtr.rows; i++) {
         for (int j = 0 ; j < blackAndWhiteImagePtr.cols; j++) {
-            if (blackAndWhiteImagePtr(i,j) > unknownIntesity) {
+            if (blackAndWhiteImagePtr(i,j) > barrierObtainedByTheMethodOfTheTriangle) {
                 blackAndWhiteImagePtr(i,j)= 255;
-                
             }else {
                 blackAndWhiteImagePtr(i,j)= 0;
             }
             }
     }
+    // simple convertation method
+    for (int i  = 0 ; i < SimpleBlackAndWhiteImagePtr.rows; i++) {
+        for (int j = 0 ; j < SimpleBlackAndWhiteImagePtr.cols; j++) {
+            if (SimpleBlackAndWhiteImagePtr(i,j) > SimpleBarrier) {
+                SimpleBlackAndWhiteImagePtr(i,j)= 255;
+            }else {
+                SimpleBlackAndWhiteImagePtr(i,j)= 0;
+            }
+        }
+    }
     
     
-    imshow("baw", blackAndWhiteImage);
+    
+    // show the pixels of the difference in 2 different methods
+    Mat ImageForShowDiff;
+    img.copyTo(ImageForShowDiff);
+    Mat_<Vec3b> newPtr = ImageForShowDiff;
+    
+//    for (int i  = 0 ; i < newPtr.rows; i++) {
+//        for (int j = 0 ; j < newPtr.cols; j++) {
+//            if (SimpleBlackAndWhiteImagePtr(i,j) == blackAndWhiteImagePtr(i,j)) {
+//                if (SimpleBlackAndWhiteImagePtr(i,j) == 255) {
+//                    //ImageForShowDiff.at<Vec3b>(i,j)=Vec3b(255,255,255);
+//                    newPtr(i,j)=Vec3b(255,255,255);
+//                }
+//                else {
+//                    //ImageForShowDiff.at<Vec3b>(i,j)=Vec3b(0,0,0);
+//                    newPtr(i,j)=Vec3b(0,0,0);
+//                }
+//            }
+//            else {
+//                //ImageForShowDiff.at<Vec3b>(i,j)=Vec3b(255,0,0);
+//                newPtr(i,j)=Vec3b(255,0,0);
+//            }
+//        }
+//    }
+    
+    
+    for (int i  = 0 ; i < newPtr.rows; i++) {
+        for (int j = 0 ; j < newPtr.cols; j++) {
+            if (j%2) {
+                newPtr(i,j)=Vec3b(255,255,255);
+            }
+            
+        }
+    }
+    imshow("CoolBaw", CoolBlackAndWhiteImage);
+    imshow("SimpleBaw",SimpleBlackAndWhiteImage);
+    imshow("Difference",ImageForShowDiff);
     waitKey(0);
     return 0;
 }
--- Black-white(lab1)/src/graytobin.cpp
+++ Black-white(lab1)/src/graytobin.cpp
@@ -1,51 +1,94 @@
-////
-////  graytobin.cpp
-////  Black-white(lab1)
-////
-////  Created by артем on 03.03.17.
-////  Copyright © 2017 артем. All rights reserved.
-////
 //
-//#include <stdio.h>
+//  graytobin.cpp
+//  Black-white(lab1)
 //
-//#include "rgbtogray.cpp"
+//  Created by артем on 03.03.17.
+//  Copyright © 2017 артем. All rights reserved.
 //
-//int tempMin = 255;
-//int temMax = 0;
-//uint *histArr = new uint[255];
-//
-//
-//// init arr
-//for (int i =0; i<256; i++) {
-//    *(histArr+i) = 0;
-//}
-//
-//void fillArray(cv::Mat_<uchar> _hist){
-//    for (int i =0; i<_hist.rows; i++) {
-//        for (int j = 0; j < _hist.cols; j++) {
-//            *(histArr+_hist(i,j))+=1;
-//        }
-//    }
-//}
-//
-//void printHistArray(cv::Mat_<uchar> _ptr){
-//    for (int i =0; i<256; i++) {
-//        cout<<*(histArr+i)<<" - "<<i<<endl;
-//    }
-//}
-//
-//int findMin(cv::Mat_<uchar> _ptr) {
-//    for (int i =0; i<256; i++) {
-//        if (*(histArr+i) < tempMin ) {
-//            tempMin = *(histArr+i);
-//        }
-//        
-//    }
-//    return tempMin;
-//}
-//
-//    
-//    
-//    
-//    
-//    
+
+#include <stdio.h>
+
+// init arr
+void initHistArr(uint *_histArr){
+    for (int i =0; i<256; i++) {
+        *(_histArr + i) = 0;
+    }
+}
+
+void fillArrayWithValuesFromGI(uint *_histArr ,Mat _grayScaleImageMatrix){
+    Mat_<uchar> grayScaleImageMatrixPtr = _grayScaleImageMatrix;
+    for (int i =0; i<_grayScaleImageMatrix.rows; i++) {
+        for (int j = 0; j < _grayScaleImageMatrix.cols; j++) {
+            *(_histArr+grayScaleImageMatrixPtr(i,j))+=1;
+        }
+    }
+}
+
+void printHistArray(uint *_histArr){
+    for (int i =0; i<256; i++) {
+        std::cout<<*(_histArr+i)<<" - "<<i<<std::endl;
+    }
+}
+
+uint findMinValueInHistArrayAndFixIndex(uint *_histArr,uint *_ptrToTheFlagVariable) {
+    uint tempMin = *(_histArr);
+    for (int i =0; i<256; i++) {
+        if (*(_histArr+i) < tempMin ) {
+            tempMin = *(_histArr+i);
+            *(_ptrToTheFlagVariable) = i;
+        }
+        
+    }
+    return tempMin;
+}
+
+uint findMaxValueInHistArrayAndFixIndex(uint* _histArr,uint *_ptrToTheFlagVariable) {
+    uint tempMax = *(_histArr);
+    for (int i =0; i<256; i++) {
+        if (*(_histArr+i) > tempMax ) {
+            tempMax = *(_histArr+i);
+            *(_ptrToTheFlagVariable) = i;
+        }
+        
+    }
+    return tempMax;
+}
+
+
+// find max distance from line that connecting 2 points to some point from the set
+
+ushort findMaxDistanceFromLineToSomePoint(int _firstPoint,int _secondPoint,uint *_histArr) {
+    int maxDistance = 0;
+    int barrierObtainedByTheMethodOfTheTriangle = -1;
+
+    for (int i = _firstPoint; i<(_secondPoint-_firstPoint); i++) {
+        double tempValue =
+        abs(
+            (_firstPoint+i) / (_secondPoint - _firstPoint)+
+            *(_histArr+_firstPoint+i) / (*(_histArr+_secondPoint) - *(_histArr+_firstPoint) ) -
+            _firstPoint / (_secondPoint-_firstPoint) +
+            *(_histArr+_firstPoint) / (*(_histArr+_secondPoint) - *(_histArr + _firstPoint) )
+            )  /
+        sqrt(
+             (1 / pow(_secondPoint - _firstPoint, 2) ) +
+             (1 / pow((*(_histArr + _secondPoint) - *(_histArr + _firstPoint)),2) )
+             );
+        if ( ((int)tempValue) > maxDistance ){
+            maxDistance = (int)tempValue;
+            barrierObtainedByTheMethodOfTheTriangle = _firstPoint + i ;
+        }
+    }
+    return barrierObtainedByTheMethodOfTheTriangle;
+}
+
+int checkCorrect(int _value,int min,int max){
+    if ( (_value <= max) && (_value >= min) ) {
+        return true;
+    }
+    else return false;
+}
+
+
+
+
+
